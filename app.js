14d0311dcddff963ef6ff444cee3f6b18087d64b
ShiKikie <toon1236@gmail.com>
2024-07-22 15:09:32 +0700

    JS output

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 function showMSGExternal($msg){
     alert($msg);
 }
+
+function myContent(){
+    const ContentCH = document.getElementById('demo');
+    ContentCH.innerHTML = "Shikikie Love You 2024"; 
+
+    document.write("Shikie 367");
+    alert("Welcome Kikie");
+    console.log("Console Kikie");
+}
